6ccdcedd714bcfbcde9779515655ed5d7ec53b18
Douglas Lessa <dougstefe@gmail.com>
2023-08-22 22:05:14 -0300

    feat: add db context

---
+++ src/Handson.Infrastructure.Data/HandsonDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Handson.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handson.Infrastructure.Data
+{
+    public class HandsonDbContext : DbContext
+    {
+        public HandsonDbContext(DbContextOptions<HandsonDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(HandsonDbContext).Assembly);
+        }
+
+        public DbSet<Student> Students => Set<Student>();
+        public DbSet<Address> Addresses => Set<Address>();
+        public DbSet<Subscription> Subscriptions => Set<Subscription>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<Lesson> Lessons => Set<Lesson>();
+        public DbSet<Course> Courses => Set<Course>();
+
+    }
+}
