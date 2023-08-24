7b0d1e687bcc87c14ad96c53477836042ccc1092
Douglas Lessa <dougstefe@gmail.com>
2023-08-23 21:54:26 -0300

    feat: add repositories definitions

---
+++ src/Handson.Domain/Repositories/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using Handson.Domain.Entities;
+
+namespace Handson.Domain.Repositories
+{
+    public interface ICourseRepository
+    {
+        Task Add(Course course);
+        Task Update(Course course);
+        Task FindById(Guid id);
+        Task FindByName(string name);
+    }
+}
---
+++ src/Handson.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using Handson.Domain.Entities;
+
+namespace Handson.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task Add(Student student);
+        Task Update(Student student);
+        Task FindById(Guid id);
+        Task FindByEmail(string email);
+    }
+}
